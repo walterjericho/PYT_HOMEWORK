1d63f4abb8db401e514741530f574c3dc07d1720
azzuddin.rizal@gmail.com <azzuddin.rizal@gmail.com>
2023-03-24 22:25:48 +0800

    variable homework

---
+++ solutions1.py
@@ -0,0 +1,51 @@
+beeps = 2
+boops = 6
+total = beeps + boops
+print(total) #this will print letter H
+
+beeps = 0
+boops = 5
+total = beeps + boops
+print(total) #this will print letter E
+
+beeps = 9
+boops = 3
+total = beeps + boops
+print(total) #this will print letter L
+
+beeps = 4
+boops = 8
+total = beeps + boops
+print(total) #this will print letter l
+
+beeps = 10
+boops = 5
+total = beeps + boops
+print(total) #this will print letter O
+
+print()
+
+beeps = 11
+boops = 12
+total = beeps + boops
+print(total)#this will print letter W
+
+beeps = 5
+boops = 5
+total = beeps + boops
+print(total) #this will print letter J
+
+beeps = 1
+boops = 17
+total = beeps + boops
+print(total) #this will print letter R
+
+beeps = 5
+boops = 7
+total = beeps + boops
+print(total) #this will print letter L
+
+beeps = 4
+boops = 0
+total = beeps + boops
+print(total) #this will print letter D
