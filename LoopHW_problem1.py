3a9f667dd3760c9440975296c6f9c0e8607d0b3d
azzuddin.rizal@gmail.com <azzuddin.rizal@gmail.com>
2023-04-07 17:12:28 +0800

    Loop Homework

---
+++ LoopHW_problem1.py
@@ -0,0 +1,10 @@
+disney_characters = ["simba", "ariel", "pumba", "flounder", "nala", "ursula", "scar", "flotsam", "timon"]
+for character in disney_characters:
+  if "u" in character:
+    print(f"{character} U are so Uniquely U!")
+  elif "i" in character:
+    print(f"{character} I bet you're Impressively Intelligent!")
+  elif "o" in character:
+    print(f"{character} O My! How Original!")
+  else:
+    print(f"{character} Ehh, a's and e's are so ordinary.")
---
+++ LoopHW_problem2.py
@@ -0,0 +1,6 @@
+temperature = 90
+
+while temperature > 75:
+  print(f"The temperature is {temperature} deg F — crank the AC!")
+  temperature -= 3
+print(f"{temperature} deg F. Ahh, that's better.")
